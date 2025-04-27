7d5e6f28d5f30d024762a518c132fff41fa89f11
yagmurgocmen <yagmurgcmn@gmail.com>
2025-04-27 13:11:19 +0300

    odev1

---
+++ odev1.kt
@@ -0,0 +1,97 @@
+package com.yagmurgocmen.odevler
+
+fun main() {
+    val sehir = "Ankara"
+    println("Sehir: "+sehir)
+    val ulke = "Turkiye"
+    println("Ulke: $ulke")
+    var telefon = "111"
+    println("Telefon: $telefon")
+    var postaKodu = "06480"
+    println("Posta Kodu: "+postaKodu)
+    val email = "yagmurgcmn@gmail.com"
+    println("Email: $email")
+    var meslek = "Ogrenci"
+    println("Meslek: "+meslek)
+    var stokMiktari = 1500
+    println("Stok Miktari: $stokMiktari")
+    var MusteriAdi = "x"
+    println("Musteri Adi: "+MusteriAdi)
+    var bakiye = 2500.54
+    println("Bakiye: $bakiye")
+    val dogumgunu = "17/06/2003"
+    println("Dogum Gunu: $dogumgunu")
+    var maas = 17000
+    println("Maas: $maas")
+    var medeniDurum = false
+    println("Medeni Durum: $medeniDurum")
+    var urunYorum = "very good"
+    println("Urun Yorumu: "+urunYorum)
+    val odemeTarihi = "03.12.2033"
+    println("Odeme Tarihi: $odemeTarihi")
+    var odeme = "kredi karti"
+    println("Odeme Sekli: $odeme")
+    var siparisAdedi = 10
+    println("Siparis Sayisi: $siparisAdedi")
+    val arabaModeli = "Audi"
+    println("Araba Modeli: $arabaModeli")
+    var kitapAdi = "Bugun Farkli Olacak"
+    println("Kitap Adi: $kitapAdi")
+    val yayinlamaTarihi = "2000"
+    println("Yayinlama Tarihi: $yayinlamaTarihi")
+    var indirimMiktari = 29
+    println("Indirim Miktari : %$indirimMiktari")
+    var odaSayisi = 5
+    println("Oda Sayisi: $odaSayisi")
+    var enlem = 12.5
+    println("Enlem: $enlem")
+    var boylam = 25
+    println("Boylam: $boylam")
+    val urunAdi = "Kitap"
+    println("Urun Adi: $urunAdi")
+    var yemekFiyati = 150
+    println("Yemek Fiyati: $yemekFiyati")
+    val marka = "mavi"
+    println("Marka: $marka")
+    val muzikadi = "The Greatest"
+    println("Muzik Adi: "+muzikadi)
+    var videosuresi = 4.5
+    println("Video Suresi: $videosuresi")
+    var urunpuani = 4.7
+    println("Urun Puani: $urunpuani")
+    val resimadi = "Ciglik"
+    println("Resim Adi: $resimadi")
+    var dosyaFormati = "pdf"
+    println("Dosya Formati: $dosyaFormati")
+    val renk = "yesil"
+    println("Renk: $renk")
+    var renkKodu = "#FFF"
+    println("Renk Kodu: $renkKodu")
+    val telefonModeli = "S23"
+    println("Telefon Modeli: $telefonModeli")
+    var ekranBoyutu = 15.6
+    println("Ekran Boyutu: $ekranBoyutu")
+    val agirlik = 2.3
+    println("Agirlik: $agirlik")
+    val ulusalGun = "23 Nisan"
+    println("Ulusal Gun: $ulusalGun")
+    val tatilGun = "1 Mayis"
+    println("Tatil Gunu: $tatilGun")
+    var rezervasyonTarihi = "28.04.2024"
+    println("Rezervasyon Tarihi: $rezervasyonTarihi")
+    val sokakAdi = "Sevgi"
+    println("Sokak Adi: $sokakAdi")
+    var otobusHatti = "330"
+    println("Otobus Hatti: $otobusHatti")
+    var kalanDakika = 12
+    println("Kalan Dakika: $kalanDakika")
+    val takipKodu = 123456789
+    println("Takip Kodu: $takipKodu")
+    var kuponSuresi = "15 dakika"
+    println("Kupon Suresi: $kuponSuresi")
+    val kuponKodu = "SEPETTE150"
+    println("Kupon Kodu: $kuponKodu")
+    var faturaAdresi = "Ev"
+    println("Fatura Adresi: $faturaAdresi")
+
+}
